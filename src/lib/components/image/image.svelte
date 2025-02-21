af7dab415f179d603ff5f627c64d20741f2e5fcf
FlavioTorres <bold217@einstein.br>
2025-02-21 02:18:23 -0300

    feat: add image component

---
+++ src/lib/components/image/image.svelte
@@ -0,0 +1,34 @@
+<script lang="ts" module>
+	import styled from 'styled-svelte5';
+
+	import DocumentsSearch from './assets/documents-search.svelte';
+
+	const VariantsMap = {
+		'documents-search': DocumentsSearch
+	};
+
+	export type Icon1Variants = keyof typeof VariantsMap;
+</script>
+
+<script lang="ts">
+	interface Props {
+		src: Icon1Variants;
+	}
+
+	const { src }: Props = $props();
+
+	const Variant = VariantsMap[src] || VariantsMap['documents-search'];
+
+	const Wrapper = styled(
+		'div',
+		() => `
+		display: flex;
+		justify-content: center;
+		align-items: center;
+  `
+	);
+</script>
+
+<Wrapper class="Image">
+	<Variant />
+</Wrapper>
